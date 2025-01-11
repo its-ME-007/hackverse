9b7aa04eb59e0b460b55c9a136c87e6f9eb46723
Anirudh <anirudh007kulkarni@gmail.com>
2025-01-11 17:59:28 +0530

    config

--- app/init.py
+++ app/init.py
@@ -6,18 +6,19 @@ from flask_jwt_extended import JWTManager
 from .routes.medicine import medicine_bp
 from .routes.appointment import appointment_bp
 from .routes.insurance import insurance_bp
+from config import Config
 # Initialize SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_object(Config)
     # Configuration settings
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        "postgresql://<username>:<password>@<host>:<port>/<database_name>"
-    )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Replace with a strong secret key
+    # app.config['SQLALCHEMY_DATABASE_URI'] = (
+    #     "postgresql://<username>:<password>@<host>:<port>/<database_name>"
+    # )
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Replace with a strong secret key
     
     # Initialize extensions
     db.init_app(app)
